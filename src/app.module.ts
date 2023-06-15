d93f0c44f15461010cba6e061692f57709776487
zeinab-hajhosseini <zeinab.hajhosseini72@gmail.com>
2023-06-15 20:13:28 +0330

    + define Lesson Entity and initialize typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { ApolloDriver } from '@nestjs/apollo';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LessonEntity } from './lesson/lesson.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [LessonEntity],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
       driver: ApolloDriver,
