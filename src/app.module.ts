376fbc1488c18167941546ad22e3aa9d4c5418fe
zeinab-hajhosseini <zeinab.hajhosseini72@gmail.com>
2023-06-15 19:28:31 +0330

    + fix Graphql driver bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
+import { ApolloDriver } from '@nestjs/apollo';
+
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
+      driver: ApolloDriver,
     }),
     LessonModule,
   ],
